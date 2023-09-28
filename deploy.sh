82aa06b98ee830aa30ec3fcb1e3f31966e9b7501
Koen <koen@pop-os.localdomain>
2023-09-28 11:26:33 +0200

    Added code signing.

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,7 @@ mkdir -p $DOCUMENT_ROOT/Kick
 cp kick.png $DOCUMENT_ROOT/Kick
 cp KickConfig.json $DOCUMENT_ROOT/Kick
 cp KickScript.js $DOCUMENT_ROOT/Kick
+sh sign.sh $DOCUMENT_ROOT/Kick/KickScript.js $DOCUMENT_ROOT/Kick/KickConfig.json
 
 # Notify Cloudflare to wipe the CDN cache
 echo "Purging Cloudflare cache for zone $CLOUDFLARE_ZONE_ID..."
